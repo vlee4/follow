04b2b263f9a5363e9f0023a986eb266a02353203
Victoria <vicglee1234007@gmail.com>
2020-09-25 14:38:04 -0500

    Updates

---
+++ precache-manifest.484008bf658b0d5ab504c05608f17e6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241edb750aea2cb2a4d3cae6968ab443",
+    "url": "/follow/index.html"
+  },
+  {
+    "revision": "a82c9d03d367ec86992b",
+    "url": "/follow/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76f9fefae44923783ce6",
+    "url": "/follow/static/js/2.bfaf6886.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/follow/static/js/2.bfaf6886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82c9d03d367ec86992b",
+    "url": "/follow/static/js/main.674bc2fb.chunk.js"
+  },
+  {
+    "revision": "9200f6fa610dff728976",
+    "url": "/follow/static/js/runtime-main.d52925fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/follow/static/media/logo.5d5d9eef.svg"
+  }
+]);
