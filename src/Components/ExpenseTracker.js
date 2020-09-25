0dd011609bfffa9ca2fe710c916abd88347910c5
Victoria <vicglee1234007@gmail.com>
2020-09-25 18:46:00 -0500

    Basic All Expenses view

--- src/Components/ExpenseTracker.js
+++ src/Components/ExpenseTracker.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Expenses from "./Expenses";
 
 function ExpenseTracker() {
   return (
-    <div>
+    <div className="tracker">
       <h2>EXPENSES</h2>
-      <div>Here are your expenses</div>
+      <div>Choose what you want to view</div>
+      <div className="trackerView">
+        <div>Expenses</div>
+        <div>Accounts</div>
+        <div>Categories</div>
+      </div>
+      <Expenses />
     </div>
   );
 }
