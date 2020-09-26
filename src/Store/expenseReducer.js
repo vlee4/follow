3d6240a8e056fd9ddfb70cb8dfd261eba48e9a96
Victoria <vicglee1234007@gmail.com>
2020-09-26 01:42:05 -0500

    Set up basics for Redux store

---
+++ src/Store/expenseReducer.js
@@ -0,0 +1,6 @@
+export default function expenseReducer(state = [], action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ src/Store/index.js
@@ -0,0 +1,18 @@
+//Redux Store
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import expenseReducer from "./expenseReducer";
+
+const reducer = combineReducers({
+  expenses: expenseReducer,
+});
+
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+
+const store = createStore(reducer, middleware);
+
+export default store;
